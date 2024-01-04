74ec79b702b78b01dbdab097bf674d56b17013ba
mbialowas10 <mbialowas@rrc.ca>
2024-01-04 09:52:08 -0600

    Initial commmit

---
+++ app/src/main/java/com/mbialowas/widgetsdemo/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.mbialowas.widgetsdemo
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.widget.doOnTextChanged
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val tv_input : TextView = findViewById(R.id.tvInput)
+        tv_input.setBackgroundColor(Color.RED)
+        tv_input.text = "Hello Class"
+        // or setText
+        tv_input.setText("Cheers")
+
+
+        val et_input : EditText = findViewById(R.id.et_input)
+
+        // get input from edit Text
+        val input = et_input.text
+
+        // register button
+        val btn : Button = findViewById(R.id.btnInput)
+        btn.setOnClickListener(){
+            //Toast.makeText(this,"Hello class, the button was click",Toast.LENGTH_LONG).show()
+            //pass the value here
+            Toast.makeText(this,"The message was ${et_input.text}",Toast.LENGTH_LONG).show()
+        }
+
+    }
+}
